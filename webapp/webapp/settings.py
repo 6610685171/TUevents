e6d5ce54c41c23f7bf1982ef95e2dc932873f3bd
Siranat Phimphicharn <6610685353@cn103>
2024-11-20 23:28:42 +0700

    fixed

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-egtzc9%z7eukd&yy=_#2p+%ku)$p_8c97ki8-vixy03)m71d$u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['TUevents.pythonanywhere.com']
 
 
 # Application definition
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ]
 import os
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "web_tu_events/static")]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'web_tu_events/staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'web_tu_events/media')
